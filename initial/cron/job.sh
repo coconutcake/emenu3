37cc83d5f2bd5506f791214d6878cffec016ad82
coconutcake <contact@mign.pl>
2021-02-22 10:19:56 +0100

    added utc time

--- initial/cron/job.sh
+++ initial/cron/job.sh
@@ -1,3 +1,7 @@
-echo "\nCron wykonuje zadanie: Wysyłka maili"
+timestamp() {
+  date +"%T" # current time
+}
+timestamp
+echo "Cron wykonuje zadanie: Wysyłka maili"
 curl -X POST -k -s https://nginx:4433/commands/email_push/ -d '{"key":"20011865a6a3a79c47e5fa3b4dd31ba9c490186d"}' -H 'Content-Type: application/json' -H 'Accept: application/json' 
 echo "KONIEC\n"
